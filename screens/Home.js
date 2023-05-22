8ca2cb51adac7daf4959b808358b677a7a75f47a
mariartve <110705321+mariartve@users.noreply.github.com>
2023-05-22 02:55:52 -0600

    Login & LogOut
    npx expo start --web --https

--- screens/Home.js
+++ screens/Home.js
@@ -1,37 +1,150 @@
 import React from "react";
-import {View, Text, TouchableOpacity} from "react-native";
+import {View, Text, TouchableOpacity, Image, StyleSheet} from "react-native";
+import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+WebBrowser.maybeCompleteAuthSession();
 
 const Home = () => {
 
     const navigation = useNavigation();
+    const [token, setToken] = useState("");
+    const [userInfo, setUserInfo] = useState(null);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        androidClientId: "636818064798-2r3ovnovmkdilj65qfnb1vsji79eider.apps.googleusercontent.com",
+        iosClientId: "636818064798-n1q6lh2qpkg4ovir2sr6ksksuumrqdib.apps.googleusercontent.com",
+        webClientId: "636818064798-vcujj7h98jh0dh31hudi5h6u248a9ts1.apps.googleusercontent.com",
+    });
+
+    const handleSignOut = async () =>  {
+        await AsyncStorage.removeItem("@user");
+        setToken(null);
+        setUserInfo(null);
+    };
+
+    useEffect(() => {
+        handleEffect();
+    }, [response, token]);
+
+    async function handleEffect() {
+    const user = await getLocalUser();
+    console.log("user", user);
+    if (!user) {
+      if (response?.type === "success") {
+       // setToken(response.authentication.accessToken);
+        getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(user);
+      console.log("loaded locally");
+    }
+    }
+
+    const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+    };
+
+    const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      console.log(error)
+    }
+    };
+
+    const ShowUserInfo = () => {
+        if (userInfo !== null) {
+            return (
+                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                    <Text style={{ fontSize: 35, fontWeight: "bold", marginBottom: 20, color: "#3DCAA6"}}>
+                        Welcome
+                    </Text>
+                    {userInfo?.picture && (
+                    <Image source={{ uri: userInfo?.picture }} style={styles.image} />
+                    )}   
+                    <Text style={{ fontSize: 25, fontWeight: "bold", marginTop: 20, marginBottom: 20, color: "#3DCAA6" }}>{userInfo.name}</Text>
+                    <TouchableOpacity style={styles.button} onPress={handleSignOut}>
+                        <Text style={styles.buttonText}>Log Out</Text>
+                    </TouchableOpacity> 
+                </View>
+            );
+        } else {
+            return null;
+        }
+    };
 
     return(
-        <View>
-            <Text>
-                Home Screen
-            </Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Stack")}
-                style={{
-                    backgroundColor:"purple",
-                    padding:10,
-                    marginTop:"20%",
-                    width:"50%",
-                    alignSelf:"center",
-                    borderRadius:10
-                }}
-            >
-                <Text
-                    style={{
-                        fontSize:15,
-                        textAlign:"center",
-                        color:"white"
-                    }}
-                >Go to Stack Screen</Text>
+        <View style={styles.container} >
+            {userInfo === null &&
+            <>
+            <Image style={styles.stretch} source={require('../media/appLogo.png')}/>
+            <Text style={{fontSize: 30, fontWeight: 'bold', color: '#3DCAA6'}}>Welcome</Text>
+            <Text style={{fontSize: 20, fontWeight: 'normal', marginBottom: 20, color: '#3DCAA6'}}>Please login</Text>
+            <TouchableOpacity style={styles.button} disabled={!request} onPress={() => {
+                    promptAsync();
+                  }}>
+                    <Text style={styles.buttonText}>Sign in with Google</Text>
             </TouchableOpacity>
+            </>}
+            {userInfo!==null && <ShowUserInfo />}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      text: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#3DCAA6",
+      },
+      image: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+      },
+    stretch: {
+      width: 300,
+      height: 200,
+      alignSelf:'center',
+      resizeMode: 'stretch',
+      marginTop: 20,
+    },
+    button: {
+      backgroundColor:"#3DCAA6",
+      padding:10,
+      marginTop:"5%",
+      width:"50%",
+      alignSelf:"center",
+      borderRadius:10
+    },
+    buttonText: {
+      fontSize:15,
+      textAlign:"center",
+      color:"white"
+    },
+});
+  
 
 export default Home;
